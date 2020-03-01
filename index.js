fe4ff120c9591688485c6bf61d36b8cd56304c46
AppDevelopmentGuy <61510426+AppDevelopmentGuy@users.noreply.github.com>
2020-03-01 15:51:04 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Below is the javascript for the side nav bar from w3schools how-to section.
+
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "20%";
+// }
+
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+// }
